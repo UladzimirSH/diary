b7d4e4a326cd18b9afbb3b74b400a414061d8513
Uladzimir Sharyi <iam@informatics.by>
2019-03-24 14:03:02 +0300

    add http healthCheck endpoint

---
+++ Diary/Controllers/HealthCheckController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Diary.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase {
+
+        [HttpGet]
+        public ActionResult Get() {
+            return Ok();
+        }
+    }
+}
